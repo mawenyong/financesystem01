ff1d1cfcd27399a100a16d2a81bc291829511c93
QiTian <1038394183@qq.com>
2019-05-22 07:36:53 +0800

    Initial commit

---
+++ src/main/java/com/finance/financesystem01/Financesystem01Application.java
@@ -0,0 +1,13 @@
+package com.finance.financesystem01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Financesystem01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Financesystem01Application.class, args);
+	}
+
+}
